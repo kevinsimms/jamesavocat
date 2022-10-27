1387ba3f2d20259fab6d934bbcba77ff18cddd63
Kevin <kevin.simms@hotmail.fr>
2022-10-27 15:46:07 +0200

    ajout 2eme version

--- home.php
+++ home.php
@@ -15,14 +15,140 @@
 include "menu.php";
 ?>
 
+<div id="firstwrap">
+
 <img src="images/mainphotohome.jpeg" alt ="photo1" class="mainphotohome1" > 
 
-<div id="secondpart">
+<div id="ftitlewrap">
+<h2 id="firsttitle">Partenaires des entrepreneurs et des entreprises</h2>
+</div>
+
+</div>
+
+
+ <div id="secondpart">
 <div id="secondpartinner">
  <p> Disponibilité, accessibilité, créativité et transparence sont au cœur de la relation qu’entretient l’équipe avec ses clients.</p>
  <p>Véritable <span id="busi">Business Partner</span>, JAMES AVOCATS les accompagne dans la durée et leur apporte au quotidien réponses et solutions sur mesure, efficaces, pragmatiques et innovantes, sur l’ensemble des thématiques qui sont au centre de la vie des affaires.</p>
   </div>
+
+  <div id="secondpartinner2">
+
+  <img src="images/table.jpeg" alt ="photo1" class="phototable" > 
 </div>
+
+
+
+</div> 
+
+
+<img src="images/photoensembleavocat.jpeg" alt ="photo1" class="photoensemble" > 
+
+
+
+<div id="fourthpart">
+
+<div id="titleoffourth">
+<h4>L'ÉQUIPE </h4>
+<p>Dédié aux entrepreneurs et aux entreprises, James Avocats est composé d'une équipe passionnée, unie, pluridisciplinaire, complémentaire et réactive</p>
+</div>
+
+
+
+<div id="posterensemble">
+
+<div id="poster1">
+<img src="images/passelac.jpeg" alt ="photo1" class="photoavocat" > 
+<h4 class="firstname">Anne-Charlotte</h4><h4 class="surname"> PASSELAC</h4>
+<h5>Avocat associée</h5>
+</div>
+
+
+
+<div id="poster1">
+<img src="images/lavergne.jpeg" alt ="photo1" class="photoavocat" > 
+<h4 class="firstname">Anne-Laure</h4><h4 class="surname"> LAVERGNE</h4>
+<h5>Avocat associée</h5>
+</div>
+
+
+
+<div id="poster1">
+<img src="images/pinet.jpeg" alt ="photo1" class="photoavocat" > 
+<h4 class="firstname">Elodie</h4><h4 class="surname"> PINET</h4>
+<h5>Avocat associée</h5>
+</div>
+
+
+
+<div id="poster1">
+<img src="images/mery.jpeg" alt ="photo1" class="photoavocat" > 
+<h4 class="firstname">Antoine</h4><h4 class="surname"> MERY</h4>
+<h5>Avocat associée</h5>
+</div>
+
+
+
+
+<div id="poster1">
+<img src="images/ousset.jpeg" alt ="photo1" class="photoavocat" > 
+<h4 class="firstname">Malike</h4><h4 class="surname"> CUSSET</h4>
+<h5>Avocat associée</h5>
+</div>
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+</div>
+
+
+
+<div id="partenairewrap">
+<h4 id="nospartenaires">Nos Partenaires </h4>
+</div>
+
+
+<div id="avocatmaispasque">
+
+<div id="maispasquetitle">  
+  <h2>AVOCATS, MAIS PAS QUE</h2>
+
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+</div>
+
+
+<div id="windowswrap">
+
+<img src="images/maispasque.jpeg" alt ="photo1" class="photomaispasque" > 
+<div id="firstwindow"></div>
+<div id="secondwindow"></div>
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+</div>
+
+
 
 
 <script src="menustyle/scriptmenu.js"></script>
